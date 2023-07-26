f9286369a8fe2e22d5a10efd2513581889358674
amanbagade <amanbagade41@gmail.com>
2023-07-26 15:50:20 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :articles
+  root 'pages#index'
+  get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'forgot_password', to: 'passwords#new', as: 'forgot_password'
+
+end
